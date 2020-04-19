0d0dbfd62bd692bd1b5a278150453eee4e04346a
Bammer <jhcse5189@gmail.com>
2020-04-19 16:10:02 +0900

    Build with clone - test as local via 'bundle exec jekyll serve'

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "github.io blog first-post",
+        "excerpt":"안녕, 제목은 ‘github.io blog first-post’이고, 마지막 수정은 ‘2020-03-29 08:54:00 -0500’에 했어.   일단 올려보는 거라서 별 말은 안적을게, 흐.   개행은 여전히 헷갈리고, categories 칭구로 subdirectory를 나타낼 수 있나봐.   그리고 excerpt 칭구는 부제같은 역할을 하는 모양이야.  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "http://localhost:4000/blog/first-post/",
+        "teaser": null
+      }]
